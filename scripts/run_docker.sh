799464856f24b56e5b273a9099007cafb36d1417
ggomezmorales <gugomog3@gmail.com>
2026-02-06 01:02:54 -0500

    chore(gz sim): gz sim first approach added

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -18,6 +18,7 @@ else
         --device /dev/dri \
         -e DISPLAY \
         -e TERM \
+        -e XDG_RUNTIME_DIR=/tmp \
         -e QT_X11_NO_MITSHM=1 \
         -e XAUTHORITY \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
